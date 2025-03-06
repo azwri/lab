40fc200dc55b115ffc24aacdd69c6fb1e11a08f2
Abdulrahman Alazwari <abdulrahman.alazwari@trendyol.com>
2025-03-07 02:17:01 +0300

    start golang projects

---
+++ go-golang-porjects/course-learning/info.md
@@ -0,0 +1,10 @@
+
+### Default Zero Values in Go
+
+In Go, variables declared without an initial value are automatically assigned their zero values:
+
+- `0` for all integer types
+- `0.0` for floating point numbers
+- `false` for booleans
+- `""` for strings
+- `nil` for interfaces, slices, channels, maps, pointers, error and functions
---
+++ go-golang-porjects/course-learning/investment_calculator/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const infaltionRate float64 = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
+
+	fmt.Print("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+infaltionRate/100, years)
+
+	fmt.Println(futureValue, futureRealValue)
+
+}
